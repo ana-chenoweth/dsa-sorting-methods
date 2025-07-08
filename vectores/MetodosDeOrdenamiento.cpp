70941d9c4dc4a8ca100547b5c7825b74aa8ec8a7
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-08 14:51:21 -0400

    Merge sort algorithm

--- vectores/MetodosDeOrdenamiento.cpp
+++ vectores/MetodosDeOrdenamiento.cpp
@@ -168,3 +168,90 @@ void QuickSort(int v[], int n, int velocidadanimacion, int izq, int der, int may
     CambiarCursor(ENCENDIDO, NORMAL);
 }
 //************************************************
+void Merge(int v[], int n, int velocidadanimacion, int inicio, int mitad, int final, int mayor)
+{
+
+    MoverCursor(0, mayor+4);
+    cout << "Ordenando...                         " << endl;
+
+    CambiarCursor(APAGADO, NORMAL);
+    int i,j, k;
+
+    int eleIzq = mitad - inicio + 1;
+    int eleDer = final - mitad;
+
+    int *VectorIzq = NULL;
+    int *VectorDer = NULL;
+
+    try{
+    VectorIzq = Crear(n);
+    VectorDer = Crear(n);
+
+            Imprimir(v,n, inicio, final, mayor);
+            if(velocidadanimacion!=0){
+                std::this_thread::sleep_for(std::chrono::milliseconds(velocidadanimacion));
+            }
+
+    for(int i = 0; i< eleIzq; i++){
+        VectorIzq[i] = v[inicio+i];
+    }
+    for(int j = 0; j< eleDer; j++){
+        VectorDer[j] = v[mitad + 1 +j];
+    }
+    i = 0;
+    j = 0;
+    k = inicio;
+
+
+    while(i < eleIzq && j < eleDer){
+        if(VectorIzq[i] <= VectorDer[j]){
+            v[k] = VectorIzq[i];
+            i++;
+        } else {
+            v[k] = VectorDer[j];
+            j++;
+        }
+        k++;
+    }
+    while(j < eleDer){
+        v[k] = VectorDer[j];
+        j++;
+        k++;
+    }
+    while(i < eleIzq){
+        v[k] = VectorIzq[i];
+        i++;
+        k++;
+    }
+
+    Destruir(VectorIzq);
+    Destruir(VectorDer);
+
+
+    }catch(const bad_alloc &){
+    cout << "Error en la asignaci\242n de memoria" <<endl;
+} catch(...){
+    cout << "Ocurrio\242 un error inesperado" << endl;
+}
+}
+//************************************************
+void MergeSort(int v[], int n, int velocidadanimacion, int inicio, int final, int mayor)
+{
+
+    MoverCursor(0, mayor+4);
+    cout << "Ordenando...                         " << endl;
+
+    CambiarCursor(APAGADO, NORMAL);
+    if(inicio < final){
+        int mitad = inicio + (final -inicio)/2;
+        MergeSort(v, n, velocidadanimacion, inicio, mitad, mayor);
+        MergeSort(v, n, velocidadanimacion, mitad+1, final, mayor);
+        Merge(v, n, velocidadanimacion, inicio, mitad, final, mayor);
+    }
+
+            Imprimir(v,n, inicio, final, mayor);
+            if(velocidadanimacion!=0){
+                std::this_thread::sleep_for(std::chrono::milliseconds(velocidadanimacion));
+            }
+}
+//*************************************************
